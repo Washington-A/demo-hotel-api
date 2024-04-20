ac957bdf4b6d784662c77f32dbeeb68dee81db14
Washington-A <washingtonarau03@gmail.com>
2024-04-20 14:04:10 -0300

    Criação da base do projeto

---
+++ src/main/java/com/example/demohotelapi/DemoHotelApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHotelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHotelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohotelapi/DemoHotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohotelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHotelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
