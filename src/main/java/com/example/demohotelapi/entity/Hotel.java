39abb69fc63db8d3b9e393cfe43d15a2edb3fe5e
Washington-A <washingtonarau03@gmail.com>
2024-04-25 12:50:34 -0300

    adição da dependencia flyway e adição de migrations

--- src/main/java/com/example/demohotelapi/entity/Hotel.java
+++ src/main/java/com/example/demohotelapi/entity/Hotel.java
@@ -5,19 +5,19 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Table(name = "hotel")
-@Entity(name = "hotel")
-@EqualsAndHashCode(of= "id")
+@Entity(name = "Hotel")
+@EqualsAndHashCode(of = "id")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Hotel {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String nome;
-    private String cidade;
-    private int qtdQuartos;
-    private boolean isDisponible;
+    private String localizacao;
 }
 
